e0631d834a46629614720868c915a5dd9a3c25b3
iamsurajbobade <iamsurajbobade@gmail.com>
2024-12-29 13:29:05 +0530

    version 1.0

--- tools/filencrypter/main.go
+++ tools/filencrypter/main.go
@@ -8,16 +8,27 @@ import (
 	"io"
 	"os"
 	"path/filepath"
+	"sort"
+	"time"
 )
 
-func encryptFile(filePath string, key []byte) error {
+func encryptFile(filePath string, key []byte, formatCount map[string]int) error {
 	inFile, err := os.Open(filePath)
 	if err != nil {
 		return err
 	}
 	defer inFile.Close()
 
-	outFile, err := os.Create(filePath + ".enc")
+	ext := filepath.Ext(filePath)
+	format := ext[1:] // Remove the dot from the extension
+	if format == "jpeg" {
+		format = "jpg"
+	}
+	formatCount[format]++
+	// todays date in format YYYYMMDD
+	datePrifix := time.Now().UTC().Format("20060102")
+	newFileName := fmt.Sprintf("%s_%s_%04d%s.enc", datePrifix, format, formatCount[format], "."+format)
+	outFile, err := os.Create(filepath.Join(filepath.Dir(filePath), newFileName))
 	if err != nil {
 		return err
 	}
@@ -54,7 +65,12 @@ func decryptFile(filePath string, key []byte) error {
 	}
 	defer inFile.Close()
 
-	outFile, err := os.Create(filePath[:len(filePath)-4])
+	// Extract the original file name without the .enc extension
+	originalFileName := filepath.Base(filePath[:len(filePath)-4])
+	// Add the dec_ prefix to the original file name
+	newFileName := "dec_" + originalFileName
+	// Create the new file path with the dec_ prefix
+	outFile, err := os.Create(filepath.Join(filepath.Dir(filePath), newFileName))
 	if err != nil {
 		return err
 	}
@@ -93,30 +109,55 @@ func main() {
 	key := make([]byte, 32)
 	copy(key, password)
 
+	var files []string
+
 	err := filepath.Walk(folder, func(path string, info os.FileInfo, err error) error {
 		if err != nil {
 			return err
 		}
 		if !info.IsDir() {
-			switch action {
-			case "encrypt":
-				if filepath.Ext(path) == ".enc" {
-					return nil
-				}
-				return encryptFile(path, key)
-			case "decrypt":
-				if filepath.Ext(path) == ".enc" {
-					return decryptFile(path, key)
-				}
-				return nil
-			}
+			files = append(files, path)
 		}
 		return nil
 	})
 
 	if err != nil {
 		fmt.Println("Error:", err)
-	} else {
-		fmt.Println("Operation completed successfully.")
+		return
 	}
+
+	sort.Sort(sort.Reverse(sort.StringSlice(files)))
+
+	fmt.Printf("Found %d valid files...\n", len(files))
+
+	formatCount := make(map[string]int)
+	processed := 0
+	for _, path := range files {
+		switch action {
+		case "encrypt":
+			if filepath.Ext(path) == ".enc" {
+				continue
+			}
+			if err := encryptFile(path, key, formatCount); err != nil {
+				fmt.Println("Error encrypting file:", err)
+			}
+			processed++
+		case "decrypt":
+			if filepath.Ext(path) == ".enc" {
+				if err := decryptFile(path, key); err != nil {
+					fmt.Println("Error decrypting file:", err)
+				}
+				processed++
+			}
+		}
+	}
+
+	fmt.Printf("Files processed: %d\n", processed)
+	if action == "encrypt" {
+		fmt.Printf("Encryption summary:\n")
+		for format, count := range formatCount {
+			fmt.Printf("  %s: %d\n", format, count)
+		}
+	}
+
 }

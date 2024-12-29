2aa65437c5139563c934327187208827166e2d07
iamsurajbobade <iamsurajbobade@gmail.com>
2024-12-29 18:14:36 +0530

    refactor. support subdirectory

--- tools/filencrypter/README.md
+++ tools/filencrypter/README.md
@@ -1,28 +1,58 @@
 # FileCrypt
 
-FileCrypt is a command-line tool to encrypt and decrypt any file format in a specified folder using strong encryption.
+FileCrypt is a command-line tool to encrypt and decrypt any file format in a specified folder using strong encryption. It maintains the subfolder structure and uses a subfolder-wise different counter for files.
+
+## Features
+
+- Encrypt and decrypt any file format and any size.
+- Strong encryption using AES-256-CBC.
+- Maintains the subfolder structure.
 
 ## Usage
 
 ```sh
-filecrypt <encrypt|decrypt> <folder> <password>
+filecrypt -m <mode/> -s <source_folder/> -d <destination_folder/> -p <password/>
 
-- <encrypt|decrypt>: Specify whether to encrypt or decrypt the files.
-- <folder>: The folder containing the files to be encrypted or decrypted.
-- <password>: The password used for encryption or decryption.
+-m: Mode of operation. Supported values are encrypt and decrypt. Default is encrypt.
+-s: Source folder. The folder containing the files to be encrypted or decrypted.
+-d: Destination folder. The folder where the encrypted or decrypted files will be saved. If not provided, it defaults to the source folder.
+-p: Password. The password used for encryption or decryption. Uses the first 32 bytes of the password.
 ```
 
 ## Example
 
-To encrypt all image and video files in the media folder with the password mysecretpassword:
+To encrypt all files in the `/path/to/original/folder` folder and save the encrypted files in the `/path/to/encrypted/folder` folder with the password `mysecretpassword`:
 
 ``` sh
-filecrypt encrypt /path/to/folder mysecretpassword
+filecrypt -m encrypt -s /path/to/original/folder -d /path/to/encrypted/folder -p mysecretpassword
 ```
 
-To decrypt all encrypted files in the media folder with the password mysecretpassword:
+To decrypt all files in the `/path/to/encrypted/folder` folder and save the decrypted files in the `/path/to/decrypted/folder` folder with the password `mysecretpassword`:
 
 ``` sh
-filecrypt decrypt /path/to/folder mysecretpassword
+filecrypt -m decrypt -s /path/to/encrypted/folder -d /path/to/decrypted/folder -p mysecretpassword
 ```
 
+## File Naming Format
+
+### Encrypted Files
+
+encrypted files are named in the format
+
+``` markdown
+YYYYMMDD_format_xxxx.format.enc
+```
+
+- YYYYMMDD: The date when the file was created/updated.
+- format: The file format (e.g., jpg, png, mp4).
+- xxxx: A subfolder-wise counter for the files of that format.
+
+### Decrypted Files
+
+decrypted files are named in the format
+
+``` markdown
+YYYYMMDD_format_xxxx.format
+```
+
+- YYYYMMDD_format_xxxx.format: The original file name before decryption.
--- tools/filencrypter/main.go
+++ tools/filencrypter/main.go
@@ -4,6 +4,7 @@ import (
 	"crypto/aes"
 	"crypto/cipher"
 	"crypto/rand"
+	"flag"
 	"fmt"
 	"io"
 	"os"
@@ -12,7 +13,117 @@ import (
 	"time"
 )
 
-func encryptFile(filePath string, key []byte, formatCount map[string]int) error {
+var (
+	mode        = flag.String("m", "encrypt", "encrypt or decrypt")
+	src         = flag.String("s", "", "source folder")
+	destination = flag.String("d", "", "destination folder")
+	pass        = flag.String("p", "", "encryption password")
+)
+
+type fileInfo struct {
+	path string
+	info os.FileInfo
+}
+
+func main() {
+
+	flag.Parse()
+
+	// Verify input parameters
+	if *mode != "encrypt" && *mode != "decrypt" {
+		fmt.Println("Invalid mode. Use 'encrypt' or 'decrypt'.")
+		return
+	}
+
+	// Check if source and password are provided
+	if *src == "" || *pass == "" {
+		fmt.Println("Missing required parameters.")
+		return
+	}
+
+	// Set destination folder to source folder if not provided
+	if *destination == "" {
+		*destination = filepath.Join(*src, "encrypted")
+	}
+
+	// Check if source folder exists
+	if _, err := os.Stat(*src); os.IsNotExist(err) {
+		fmt.Println("Source folder does not exist.")
+		return
+	}
+	// Check if destination folder exists
+	if _, err := os.Stat(*destination); os.IsNotExist(err) {
+		fmt.Println("Destination folder does not exist.")
+		if err := os.MkdirAll(*destination, 0755); err != nil {
+			fmt.Println("Error creating destination folder:", err)
+			return
+		}
+	}
+
+	key := make([]byte, 32)
+	copy(key, *pass)
+
+	var files []fileInfo
+
+	err := filepath.Walk(*src, func(path string, info os.FileInfo, err error) error {
+		if err != nil {
+			return err
+		}
+		if !info.IsDir() {
+			files = append(files, fileInfo{path, info})
+		}
+		return nil
+	})
+
+	if err != nil {
+		fmt.Println("Error:", err)
+		return
+	}
+
+	sort.Slice(files, func(i, j int) bool { // Sort files by modification time (oldest first)
+		return files[i].info.ModTime().Before(files[j].info.ModTime())
+	})
+
+	fmt.Printf("Found %d valid files...\n", len(files))
+	for _, path := range files {
+		fmt.Println(path)
+	}
+
+	formatCount := make(map[string]map[string]int)
+	processed := 0
+	for _, file := range files {
+		switch *mode {
+		case "encrypt":
+			if filepath.Ext(file.path) == ".enc" {
+				continue
+			}
+			if err := encryptFile(file.path, *src, *destination, key, formatCount); err != nil {
+				fmt.Println("Error encrypting file:", err)
+			}
+			processed++
+		case "decrypt":
+			if filepath.Ext(file.path) == ".enc" {
+				if err := decryptFile(file.path, *src, *destination, key); err != nil {
+					fmt.Println("Error decrypting file:", err)
+				}
+				processed++
+			}
+		}
+	}
+
+	fmt.Printf("Files processed: %d\n", processed)
+	if *mode == "encrypt" {
+		fmt.Printf("Encryption summary:\n")
+		for subfolder, formats := range formatCount {
+			fmt.Printf("Subfolder: %s\n", subfolder)
+			for format, count := range formats {
+				fmt.Printf("  %s: %d\n", format, count)
+			}
+		}
+	}
+}
+
+func encryptFile(filePath string, src string, destination string, key []byte, formatCount map[string]map[string]int) error {
 	inFile, err := os.Open(filePath)
 	if err != nil {
 		return err
@@ -20,15 +131,44 @@ func encryptFile(filePath string, key []byte, formatCount map[string]int) error 
 	defer inFile.Close()
 
 	ext := filepath.Ext(filePath)
+	if ext == "" {
+		return fmt.Errorf("file %s has no extension", filePath)
+	}
 	format := ext[1:] // Remove the dot from the extension
 	if format == "jpeg" {
 		format = "jpg"
 	}
-	formatCount[format]++
-	// todays date in format YYYYMMDD
-	datePrifix := time.Now().UTC().Format("20060102")
-	newFileName := fmt.Sprintf("%s_%s_%04d%s.enc", datePrifix, format, formatCount[format], "."+format)
-	outFile, err := os.Create(filepath.Join(filepath.Dir(filePath), newFileName))
+
+	// Create the destination subfolder structure
+	relPath, err := filepath.Rel(src, filePath)
+	if err != nil {
+		return err
+	}
+	subfolder := filepath.Dir(relPath)
+	destPath := filepath.Join(destination, subfolder)
+	if err := os.MkdirAll(destPath, 0755); err != nil {
+		return err
+	}
+
+	// Initialize the format count map for the subfolder if not already initialized
+	if _, exists := formatCount[subfolder]; !exists {
+		formatCount[subfolder] = make(map[string]int)
+	}
+	formatCount[subfolder][format]++
+
+	// get file creation date
+	datePrefix := time.Now().UTC().Format("20060102")
+	inFileInfo, err := os.Stat(filePath)
+	if err == nil {
+		datePrefix = inFileInfo.ModTime().UTC().Format("20060102")
+	}
+	//creationTime := inFileInfo.ModTime().UTC().Format("20060102")
+
+	// Today's date in format YYYYMMDD
+	//datePrefix := time.Now().UTC().Format("20060102")
+	newFileName := fmt.Sprintf("%s_%s_%04d%s.enc", datePrefix, format, formatCount[subfolder][format], "."+format)
+
+	outFile, err := os.Create(filepath.Join(destPath, newFileName))
 	if err != nil {
 		return err
 	}
@@ -58,7 +198,7 @@ func encryptFile(filePath string, key []byte, formatCount map[string]int) error 
 	return nil
 }
 
-func decryptFile(filePath string, key []byte) error {
+func decryptFile(filePath string, src string, destination string, key []byte) error {
 	inFile, err := os.Open(filePath)
 	if err != nil {
 		return err
@@ -68,9 +208,21 @@ func decryptFile(filePath string, key []byte) error {
 	// Extract the original file name without the .enc extension
 	originalFileName := filepath.Base(filePath[:len(filePath)-4])
 	// Add the dec_ prefix to the original file name
-	newFileName := "dec_" + originalFileName
+	// newFileName := "dec_" + originalFileName
+
+	// Create the destination subfolder structure
+	relPath, err := filepath.Rel(src, filePath)
+	if err != nil {
+		return err
+	}
+	subfolder := filepath.Dir(relPath)
+	destPath := filepath.Join(destination, subfolder)
+	if err := os.MkdirAll(destPath, 0755); err != nil {
+		return err
+	}
+
 	// Create the new file path with the dec_ prefix
-	outFile, err := os.Create(filepath.Join(filepath.Dir(filePath), newFileName))
+	outFile, err := os.Create(filepath.Join(destPath, originalFileName))
 	if err != nil {
 		return err
 	}
@@ -94,70 +246,4 @@ func decryptFile(filePath string, key []byte) error {
 	}
 
 	return nil
-}
-
-func main() {
-	if len(os.Args) < 4 {
-		fmt.Println("Usage: filecrypt <encrypt|decrypt> <folder> <password>")
-		return
-	}
-
-	action := os.Args[1]
-	folder := os.Args[2]
-	password := os.Args[3]
-
-	key := make([]byte, 32)
-	copy(key, password)
-
-	var files []string
-
-	err := filepath.Walk(folder, func(path string, info os.FileInfo, err error) error {
-		if err != nil {
-			return err
-		}
-		if !info.IsDir() {
-			files = append(files, path)
-		}
-		return nil
-	})
-
-	if err != nil {
-		fmt.Println("Error:", err)
-		return
-	}
-
-	sort.Sort(sort.Reverse(sort.StringSlice(files)))
-
-	fmt.Printf("Found %d valid files...\n", len(files))
-
-	formatCount := make(map[string]int)
-	processed := 0
-	for _, path := range files {
-		switch action {
-		case "encrypt":
-			if filepath.Ext(path) == ".enc" {
-				continue
-			}
-			if err := encryptFile(path, key, formatCount); err != nil {
-				fmt.Println("Error encrypting file:", err)
-			}
-			processed++
-		case "decrypt":
-			if filepath.Ext(path) == ".enc" {
-				if err := decryptFile(path, key); err != nil {
-					fmt.Println("Error decrypting file:", err)
-				}
-				processed++
-			}
-		}
-	}
-
-	fmt.Printf("Files processed: %d\n", processed)
-	if action == "encrypt" {
-		fmt.Printf("Encryption summary:\n")
-		for format, count := range formatCount {
-			fmt.Printf("  %s: %d\n", format, count)
-		}
-	}
-
 }
